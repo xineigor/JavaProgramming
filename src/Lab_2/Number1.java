8399dd93d4ee2807b7b44e9756cccd3c6b3b8996
wyvern <triazeya@gmail.com>
2023-10-21 10:51:04 +0300

    end3

--- src/Lab_2/Number1.java
+++ src/Lab_2/Number1.java
@@ -26,7 +26,7 @@ public class Number1 {
         int time1_int = Integer.parseInt(time1);
         int time2_int = Integer.parseInt(time2);
 
-        //лперации с данными
+        //операции с данными
         int time1_int_minuet = time1_int * 60; //перевод в минуты
         int sum_time = time1_int_minuet + time2_int; // время прибытия в минутах
         int time_way = sum_time - way; // время пути в минутах
@@ -43,44 +43,25 @@ public class Number1 {
             format22_time = "0" + format22_time;
         }
 
-        //System.out.println("Форматы");
-        //System.out.println(format11_time);
-        //System.out.println(format22_time);
-
         boolean minus_check_1 = format11_time.contains("-"); //проверка выхождения за 00:00
         boolean minus_check_2 = format22_time.contains("-");
 
         int format11_time_int = Integer.parseInt(format11_time);
         int format22_time_int = Integer.parseInt(format22_time);
 
-        //int result1;
-        // int result2;
+
         if (!((minus_check_2) || (minus_check_1))) {
             System.out.println(format11_time+':'+format22_time); //вывод обычный без выхода за 00:00
-            //System.out.println(format11_time);
-            //System.out.println(format22_time);
         }
         if ((minus_check_2) || (minus_check_1)) { //перевод в минуты чтобы высчитать выход за 00:00
 
             int result_format = 1440 + format22_time_int;
             int format11_time_int_min = format11_time_int * 60;
             result_format = result_format + format11_time_int_min;
-
             int result_format1 = result_format / 60;
             int result_format2 = result_format % 60;
-            //int resa1;
-            //int resa2;
-            //if (way > 60){
-                //resa1 = 24 + format11_time_int;
-                //resa2 = format22_time_int;
-                //System.out.println(resa1);
-                //System.out.println(resa2);
-            //}
 
             System.out.println(result_format1+":"+result_format2); //вывод с выходом за 00:00
-            //System.out.println(result_format1);
-            //System.out.println(result_format2);
-            //System.out.println(result_format);
         }
 
 
