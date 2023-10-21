790b3277fde9f3f3b6bddeb40b7a14b4cec760fe
wyvern <triazeya@gmail.com>
2023-10-22 01:40:54 +0300

    Подправить большую букву с scan close

--- src/Lab_2/Number2.java
+++ src/Lab_2/Number2.java
@@ -1,7 +1,68 @@
 package Lab_2;
 
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+
 public class Number2 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Fruit :");
+        String Fruit = scan.nextLine();
+
+        System.out.println("Day of the week: ");
+        String day_week = scan.nextLine();
+
+        System.out.println("Quantity kg: ");
+        float kg = scan.nextFloat();
+
+        double Price = 0;
+        boolean is_weekend;
+
+        if ((day_week.equals("Saturday")) || (day_week.equals("Sunday"))) {
+            is_weekend = true;
+        } else {
+            is_weekend = false;
+        }
+
+        switch (Fruit) {
+            case ("apple"):
+                Price = 9.55 * 100 * kg;
+                break;
+            case ("plum"):
+                Price = 10.98 * 100 * kg;
+                break;
+            case ("banana"):
+                Price = 11.49 * 100 * kg;
+
+                if (is_weekend){
+                    Price = Price * 0.91;
+                }
+
+                break;
+            case ("grapes"):
+                Price = 15.99 * 100 * kg;
+
+                if (!(is_weekend)){
+                    Price = Price * 0.91;
+                }
+
+                break;
+            case ("orange"):
+                Price = 17.99 * 100 * kg;
+                break;
+            case ("kiwi"):
+                Price = 37.99 * 100 * kg;
+                break;
+            case ("mango"):
+                Price = 39.99 * 100 * kg;
+                break;
+            default:
+                System.out.println("NO");
+                break;
+
+        }
+        System.out.println(Price);
     }
 }
