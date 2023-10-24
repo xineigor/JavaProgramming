d415f42d1cd7d53a04ef6d5aaf1d377cf1057f3d
wyvern <triazeya@gmail.com>
2023-10-24 14:06:55 +0300

    Commit num 2 lub 1.1

--- src/Lab_1/Number2.java
+++ src/Lab_1/Number2.java
@@ -9,15 +9,24 @@ public class Number2 {
         System.out.println("Enter String: ");
         String str_in = scan.nextLine();
 
+        int index_end = str_in.length();
+
         String ch1 = scan.nextLine();  //считывание значений
         String ch2 = scan.nextLine();
 
         int index1 = str_in.indexOf(ch1); //индекс нужных значений
         int index2 = str_in.indexOf(ch2);
 
-        String ch1_upper_case = str_in.substring(index1, index1).toUpperCase(); //нужные значения по индексу в строку и перевод
-        String ch2_upper_case = str_in.substring(index2, index2).toUpperCase();
+        String ch1_upper_case = str_in.substring(index1, index1+1).toUpperCase(); //нужные значения по индексу в строку и перевод
+        String ch2_upper_case = str_in.substring(index2, index2+1).toUpperCase();
 
+        String stroka_end = ch1_upper_case + stroka2 + ch2_upper_case;
+
+//        String stroka1 = str_in.substring(0, index1);
+//        String stroka2 = str_in.substring(index1+1, index2);
+//        String stroka3 = str_in.substring(index2+1, index_end);
+//
+//        String stroka_end = stroka1 + ch1_upper_case + stroka2 + ch2_upper_case + stroka3;
 
         //StringBuffer str_in_sb = new StringBuffer(str_in);
         //str_in_sb.setCharAt(0, 'c');
@@ -34,7 +43,9 @@ public class Number2 {
 //        result = str_in.substring(1, str_in.length() - 1);
 
 
-        System.out.println(ch1_upper_case);
-        System.out.println(ch2_upper_case);
+//        System.out.println(ch1_upper_case);
+//        System.out.println(ch2_upper_case);
+
+        System.out.println(stroka_end);
     }
 }
