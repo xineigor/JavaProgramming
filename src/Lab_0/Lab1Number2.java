e4015877c29f78fc912603b8b7f160d4713b77d5
wyvern <triazeya@gmail.com>
2023-10-25 22:55:45 +0300

    stop

--- src/Lab_0/Lab1Number2.java
+++ src/Lab_0/Lab1Number2.java
@@ -2,7 +2,7 @@ package Lab_0;
 
 import java.util.Scanner;
 
-public class Number2Lab1 {
+public class Lab1Number2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         char symbol = scan.next().charAt(0);
