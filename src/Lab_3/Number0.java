b0044f4c8099a04f1667c08fa6553b3636005c6f
wyvern <triazeya@gmail.com>
2023-10-28 10:07:24 +0300

    q

---
+++ src/Lab_3/Number0.java
@@ -0,0 +1,17 @@
+package Lab_3;
+
+import java.util.Scanner;
+
+public class Number0 {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner scan = new Scanner(System.in);
+        int enter = 0;
+
+        do {
+            sum += enter;
+            enter = Integer.parseInt(scan.nextLine());
+        } while (enter <= 17);
+        System.out.println(sum);
+    }
+}
--- src/Lab_3/Number1.java
+++ src/Lab_3/Number1.java
@@ -2,6 +2,5 @@ package Lab_3;
 
 public class Number1 {
     public static void main(String[] args) {
-        
     }
 }
