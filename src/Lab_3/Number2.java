bf0830b6693fe4ce8f836581bd2908c996becc03
wyvern <triazeya@gmail.com>
2023-11-04 01:22:12 +0300

    N2L3

--- src/Lab_3/Number2.java
+++ src/Lab_3/Number2.java
@@ -3,6 +3,21 @@ package Lab_3;
 import java.util.Scanner;
 
 public class Number2 {
-    Scanner scan = new Scanner(System.in); // //
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int amount_numbers = Integer.parseInt(scan.nextLine());
 
+        int number_max = 0;
+        int number = 0;
+
+        while (number < amount_numbers) {
+            int inn = scan.nextInt();
+            number++;
+            if ((inn > number_max) && (inn % 3 == 0)) {
+                number_max = inn;
+            }
+        }
+
+        System.out.println(number_max);
+    }
 }
