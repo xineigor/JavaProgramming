bf0e0ad959c8d167e09f0e8b5887e30b98093898
wyvern <triazeya@gmail.com>
2023-11-04 01:40:34 +0300

    N2L3

---
+++ src/Lab_4/Number0_1.java
@@ -0,0 +1,20 @@
+package Lab_4;
+
+import java.util.Scanner;
+
+public class Number0_1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner scan = new Scanner(System.in);
+        int start = Integer.parseInt(scan.next());
+        int finish = Integer.parseInt(scan.next());
+
+        for (int i = start; i <= finish; i++ ) {
+            if (i % 3 == 0) continue;
+            if (i == 17) continue;
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
