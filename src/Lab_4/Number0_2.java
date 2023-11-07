b6a072318aec47c8979115d8279d80c23cf01d79
wyvern <triazeya@gmail.com>
2023-11-07 03:26:07 +0300

    L4

--- src/Lab_4/Number0_2.java
+++ src/Lab_4/Number0_2.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class Number0_2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         int start = Integer.parseInt(scan.next());
         int finish = Integer.parseInt(scan.next());
         int sum = 0;
+
         for (int i = start; i <= finish; i++) {
             int factorial = i;
+
             for (int j = factorial -1; j > 0; --j) {
                 factorial *= j;
             }
 
             sum += factorial;
         }
+
         System.out.println(sum);
     }
 }
