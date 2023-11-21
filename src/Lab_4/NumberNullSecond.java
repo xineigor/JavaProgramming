acbaf3651a10151483b679424f2ed6738cbf1870
wyvern <triazeya@gmail.com>
2023-11-21 12:48:52 +0300

    inclass

--- src/Lab_4/NumberNullSecond.java
+++ src/Lab_4/NumberNullSecond.java
@@ -5,26 +5,27 @@ import java.util.Scanner;
 public class NumberNullSecond {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         int start = Integer.parseInt(scan.next());
         int finish = Integer.parseInt(scan.next());
         int sum = 0;
-        int fc =0;
+        long fc = 1;
 
         for (int i = start; i <= finish; i++) {
-            int factorial = i;
 
-            for (int j = factorial -1; j > 0; --j) {
+            if (i == 17){ continue; }
+
+            int factorial = i;
+            for (int j = factorial - 1; j > 0; --j) {
                 factorial *= j;
             }
 
-            if (factorial == 17){
-                continue;
-            }
             sum += factorial;
         }
 
-
+        for (long k = 2; k <= 17; k++) {
+            fc = fc * k;
+            System.out.println(fc);
+        }
 
         System.out.println(fc);
         System.out.println(sum);
