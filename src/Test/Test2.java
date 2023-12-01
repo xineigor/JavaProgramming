c03dfc27f9781157fc73a70b77fe595ae949ae61
wyvern <triazeya@gmail.com>
2023-12-02 01:18:32 +0300

    l

---
+++ src/Test/Test2.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        System.out.println(sum(a, b));
+    }
+
+    static int sum(int a, int b) { //метод
+        int s = a + b;
+        return s;
+    }
+}
