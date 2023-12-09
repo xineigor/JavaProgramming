4b544b13d721e40f9afc4478ae78511920f4e9ba
wyvern <triazeya@gmail.com>
2023-12-09 23:08:05 +0300

    l

--- src/Lab_7/NumberOne.java
+++ src/Lab_7/NumberOne.java
@@ -6,18 +6,40 @@ public class NumberOne {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int R = Integer.parseInt(scan.nextLine());
-        int C = Integer.parseInt(scan.nextLine());
+        int N = Integer.parseInt(scan.nextLine());
+        int R = Integer.parseInt(scan.nextLine()); //строк
+        int C = Integer.parseInt(scan.nextLine()); //столбцов
 
-        int matrix[][] = new int[R][C];
+        int[][] matrix = new int[R][C];
 
-        
+//        R -= 1;
+//        C -= 1;
 
-        for(int[] x : matrix){
-            System.out.println();
-            for(int y: x){
-                System.out.print(y + " ");
+        int k = 1;
+        for (int i = C; i >= 1; i--){
+            k = k * -1;
+            for (int j = R ; j >= 1; j = j + k){
+                matrix[j-1][i-1] = N;
+                N++;
+
+                for(int[] x : matrix){
+                    System.out.println();
+                    for(int y: x){
+                        System.out.print(y + " ");
+                    }
+                }
             }
         }
+
+//        for(int i = C  ; i > 0; i--){
+//        matrix[R-1][C-1] = N;
+        
+
+//        for(int[] x : matrix){
+//            System.out.println();
+//            for(int y: x){
+//                System.out.print(y + " ");
+//            }
+//        }
     }
 }
---
+++ src/Lab_7/NumberOneDo.java
@@ -0,0 +1,49 @@
+package Lab_7;
+
+import java.util.Scanner;
+
+public class NumberOneDo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = Integer.parseInt(scan.nextLine());
+        int R = Integer.parseInt(scan.nextLine()); //строк
+        int C = Integer.parseInt(scan.nextLine()); //столбцов
+
+        int[][] matrix = new int[R][C];
+
+        int k = 1;
+        for (int i = C; i >= 1; i--){
+            k = k * -1;
+            if (k == -1) {
+                for (int j = R; j >= 1; j--) {
+                    matrix[j - 1][i - 1] = N;
+                    N++;
+
+                    for (int[] x : matrix) {
+                        System.out.println();
+                        for (int y : x) {
+                            System.out.print(y + " ");
+                        }
+                    }
+                }
+            }
+
+            if (k == 1){
+                for (int j = 1; j <= R; j++) {
+                    matrix[j - 1][i - 1] = N;
+                    N++;
+
+                    for (int[] x : matrix) {
+                        System.out.println();
+                        for (int y : x) {
+                            System.out.print(y + " ");
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
