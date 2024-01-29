d2b7b02d6eb3bc2e975f90c6f612f634ac1d0bc1
wyvern <triazeya@gmail.com>
2024-01-29 06:08:34 +0300

    l

--- src/Lab_4/NumberTwo.java
+++ src/Lab_4/NumberTwo.java
@@ -1,7 +1,38 @@
 package Lab_4;
 
+import java.util.Scanner;
+
 public class NumberTwo {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean check = true;
+        String destinationСountry = scan.nextLine();
+        int amountMoneyTrip = Integer.parseInt(scan.nextLine());
 
+
+        String amountSavings = "0";
+
+        int sumSavings = 0;
+
+
+
+
+        while (check) {
+
+            if (sumSavings >= amountMoneyTrip) {  //( if (amountSavings.matches("[a-zA-Z]+") || (sumSavings >= amountMoneyTrip))
+                System.out.println("Going to " + destinationСountry);
+                sumSavings = 0;
+                destinationСountry = scan.nextLine();
+                amountMoneyTrip = Integer.parseInt(scan.nextLine());
+
+            amountSavings = scan.nextLine();
+
+            sumSavings += Integer.parseInt(amountSavings);
+
+
+            }
+
+
+        }
     }
 }
