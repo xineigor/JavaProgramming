79f7f75f002174cf82309c7935a46b7bca2f50e8
wyvern <triazeya@gmail.com>
2024-03-02 01:45:59 +0300

    l

--- src/Lab_7/NumberTwo.java
+++ src/Lab_7/NumberTwo.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class NumberTwo {
     public static int arraySize;
+//    public static int sumElementsPartO = 0;
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
@@ -11,33 +12,61 @@ public class NumberTwo {
 
         int[][] mainArray = NumberTwo.enterArrayS();
 
+        int sumElementsPart = 0;
+
+//        for (int i = 0; i < arraySize; i++) {
+//            sumElementsPart += mainArray[0][0];
+//            sumElementsPart += mainArray[0][arraySize/2-1];
+//            sumElementsPart += mainArray[1][0];
+//            sumElementsPart += mainArray[2]
+//
+//        }
+
         for (int[] x : mainArray) {
             System.out.println();
             for (int y : x) {
                 System.out.print(y + "\t"); //todo лишние прбела
             }
         }
+
+//        System.out.println(sumElementsPartO);
     }
 
     public static int[][] enterArrayS() {  //мето возвращающий из стрики через пробел чисел, массив
         Scanner scan = new Scanner(System.in); //todo вввести размерность массива вначале
 
         String numbersString = scan.nextLine();
+        int sumElementsPartT = 0;
+
 
         int indexArrayX = 0;
         int indexArrayY = 0;
+        int numberSpaces = 0;
         int i = 0;
         int[][] returnArray = new int[arraySize][arraySize];
-        int amountNumbers = numbersString.length() / 2;
+        int amountNumbers = numbersString.length();
         String temp;
 
         while (i < amountNumbers) {
 
+            if (numberSpaces > (arraySize * arraySize) - 1) {
+                break;
+            }
+
             temp = numbersString.substring(i, i + 1);
+
             if (temp.equals(" ")){
+                numberSpaces += 1;
+//                if (numberSpaces == ((arraySize/2) * (arraySize/2))) {
+//                    sumElementsPartO = sumElementsPartT;
+//                    sumElementsPartT = 0;
+//                }
+
+                i += 1;
                 continue;
             }
-//            System.out.println(temp);
+//            sumElementsPartT = Integer.parseInt(numbersString.substring(i, i + 1));
+////            System.out.println(temp);
             returnArray[indexArrayY][indexArrayX] = Integer.parseInt(temp);
             indexArrayX += 1;
 
@@ -46,8 +75,8 @@ public class NumberTwo {
                 indexArrayY += 1;
             }
 
-            i += 2;
-            System.out.println(i);
+            i += 1;
+//            System.out.println(i);
         }
 
         return returnArray;
