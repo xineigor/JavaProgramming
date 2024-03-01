32f059ac9abb15d342185e7003f25747d283d027
wyvern <triazeya@gmail.com>
2024-03-02 01:15:09 +0300

    l

--- src/Lab_7/NumberTwo.java
+++ src/Lab_7/NumberTwo.java
@@ -3,7 +3,12 @@ package Lab_7;
 import java.util.Scanner;
 
 public class NumberTwo {
+    public static int arraySize;
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        arraySize = scan.nextInt();
+
         int[][] mainArray = NumberTwo.enterArrayS();
 
         for (int[] x : mainArray) {
@@ -16,8 +21,9 @@ public class NumberTwo {
 
     public static int[][] enterArrayS() {  //мето возвращающий из стрики через пробел чисел, массив
         Scanner scan = new Scanner(System.in); //todo вввести размерность массива вначале
-        int arraySize = scan.nextInt();
+
         String numbersString = scan.nextLine();
+
         int indexArrayX = 0;
         int indexArrayY = 0;
         int i = 0;
@@ -25,19 +31,23 @@ public class NumberTwo {
         int amountNumbers = numbersString.length() / 2;
         String temp;
 
-        while (i < amountNumbers * 2) {
+        while (i < amountNumbers) {
 
             temp = numbersString.substring(i, i + 1);
-            System.out.println(temp);
+            if (temp.equals(" ")){
+                continue;
+            }
+//            System.out.println(temp);
             returnArray[indexArrayY][indexArrayX] = Integer.parseInt(temp);
             indexArrayX += 1;
 
-            if (indexArrayX > 3) {
+            if (indexArrayX > arraySize - 1) {
                 indexArrayX = 0;
                 indexArrayY += 1;
             }
 
             i += 2;
+            System.out.println(i);
         }
 
         return returnArray;
