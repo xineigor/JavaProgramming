ce6deaab7bd707bdf503c6c86ccd6cdb45a3a208
wyvern <triazeya@gmail.com>
2024-03-09 03:27:46 +0300

    L7N2Null

--- src/Lab_7/NumberNullSecond.java
+++ src/Lab_7/NumberNullSecond.java
@@ -1,5 +1,7 @@
 package Lab_7;
 
+import Service.Service;
+
 import java.util.Arrays;
 
 public class NumberNullSecond {
@@ -12,13 +14,23 @@ public class NumberNullSecond {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {;
                 for (int l = 0; l < k; l++) {
-                    trio[i][j][l] = i + j + l;
+                    trio[i][j][l] = (i + 1) * (j + 1) * (l + 1);
                 }
 
             }
         }
 
-        //System.out.println(Arrays.toString(trio));
-        System.out.println(Arrays.deepToString(trio[0]));
+        for (int i = 0; i < n; i++) {
+            printMatrix(trio[i]);
+            System.out.println();
+        }
+    }
+    public static void printMatrix(int[][] matrix) {
+        for (int[] x : matrix) {
+            for (int y : x) {
+                System.out.print(y + "\t");
+            }
+            System.out.println();
+        }
     }
 }
