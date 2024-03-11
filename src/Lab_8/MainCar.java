633263555f80695e4df07b2102145285fbf7f5a0
wyvern <triazeya@gmail.com>
2024-03-11 12:20:52 +0300

    C

---
+++ src/Lab_8/MainCar.java
@@ -0,0 +1,8 @@
+package Lab_8;
+
+public class MainCar {
+    public static void main(String[] args) {
+        Car car = new Car("Ger", "Mer", 300);
+        System.out.println(car.getInfo());
+    }
+}
